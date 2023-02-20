6f01af74435b2642d12c1ed7ba9690e44574a7dc
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-02-20 19:47:47 -0300

    arreglando las cards de productos, usando la funcion de descuento

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,20 +4,18 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");/* Una funcion  */
 
 const controller = {
 	index: (req, res) => {
 		// Do the magic
-		let productsInSale = products.filter(product => product.category === "in-sale");
-		let productsVisited = products.filter(product => product.category === "visited");
-
-		res.render("index", {
-			title: "Mercado Liebre",
-			productsVisited,
-			productsInSale,
+		let ofertas = products.filter(product => product.category === "in-sale");/* nos pide que mandemos las variable en dos partes  */
+		let visitados = products.filter(product => product.category === "visited");
+		res.render('index', {
+			ofertas,
+			visitados,
 			toThousand
-		})
+		});
 	},
 	search: (req, res) => {
 		// Do the magic
